efa9fdc2c17c367074ac5e54fa5dae7810a9b199
RotcivOcnarb <rotciv_ocnarb@hotmail.com>
2023-04-18 20:35:52 -0300

    savesystem surrogate fix

--- Runtime/SaveSystem/Scripts/SaveSystem.cs
+++ Runtime/SaveSystem/Scripts/SaveSystem.cs
@@ -9,6 +9,35 @@ namespace Rotslib.Saving {
         public static bool logFilePath;
         public static void SaveGame<T>(T data) {
             BinaryFormatter bf = new BinaryFormatter();
+            bf.SurrogateSelector = GetSurrogates();
+
+            if (Debug.isDebugBuild && logFilePath) {
+                Debug.Log("Save path: " + Application.persistentDataPath);
+            }
+            FileStream file = File.Open(Application.persistentDataPath + "/save.dat", FileMode.OpenOrCreate);
+            bf.Serialize(file, data);
+            file.Close();
+        }
+
+        public static bool LoadGame<T>(out T data) {
+            try {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.SurrogateSelector = GetSurrogates();
+
+                using (FileStream file = File.Open(Application.persistentDataPath + "/save.dat", FileMode.OpenOrCreate)) {
+                    data = (T)bf.Deserialize(file);
+                }
+                return true;
+            }
+            catch (SerializationException e) {
+                Debug.Log(e);
+                data = default;
+                return false;
+            }
+        }
+
+        private static SurrogateSelector GetSurrogates()
+        {
             SurrogateSelector surrogateSelector = new SurrogateSelector();
 
             //Vector3 surrogate
@@ -23,29 +52,7 @@ namespace Rotslib.Saving {
             QuaternionSerializationSurrogate quaternionSS = new QuaternionSerializationSurrogate();
             surrogateSelector.AddSurrogate(typeof(Quaternion), new StreamingContext(StreamingContextStates.All), quaternionSS);
 
-            bf.SurrogateSelector = surrogateSelector;
-
-            if (Debug.isDebugBuild && logFilePath) {
-                Debug.Log("Save path: " + Application.persistentDataPath);
-            }
-            FileStream file = File.Open(Application.persistentDataPath + "/save.dat", FileMode.OpenOrCreate);
-            bf.Serialize(file, data);
-            file.Close();
-        }
-
-        public static bool LoadGame<T>(out T data) {
-            try {
-                BinaryFormatter bf = new BinaryFormatter();
-                using (FileStream file = File.Open(Application.persistentDataPath + "/save.dat", FileMode.OpenOrCreate)) {
-                    data = (T)bf.Deserialize(file);
-                }
-                return true;
-            }
-            catch (SerializationException e) {
-                Debug.Log(e);
-                data = default;
-                return false;
-            }
+            return surrogateSelector;
         }
 
     }
