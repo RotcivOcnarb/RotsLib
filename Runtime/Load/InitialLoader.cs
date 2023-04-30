218f9151b2b0bd8a75b6367dca825163c8282e2f
RotcivOcnarb <rotciv_ocnarb@hotmail.com>
2023-04-29 22:13:05 -0300

    intial loader can wait

--- Runtime/Load/InitialLoader.cs
+++ Runtime/Load/InitialLoader.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Rotslib.Load {
@@ -5,10 +6,12 @@ namespace Rotslib.Load {
 
         public static bool hasLoaded;
         public static string debugScene;
+        public static bool waiting;
 
         [Attributes.Scene]
         public string mainScene;
 
+        
         void LoadInitialScene(string scene) {
             if (!string.IsNullOrEmpty(debugScene)) {
                 Transition.Transition.Goto(debugScene);
@@ -23,7 +26,8 @@ namespace Rotslib.Load {
             Application.targetFrameRate = 60;
         }
 
-        private void Start() {
+        private IEnumerator Start() {
+            while (waiting) yield return new WaitForEndOfFrame();
             LoadInitialScene(mainScene);
         }
     }
